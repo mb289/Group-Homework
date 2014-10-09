abef346eb37d681deae1ee3696a0acda2fc60a92
Marco Bernal Jr <bernalmjr@Marcos-MacBook-Air.local>
2014-10-09 12:33:21 -0500

    update

---
+++ hw.js
@@ -0,0 +1,9 @@
+var beer = [
+    { beername : 'Murphys Law',  Type : 'German-style Brown Ale'},
+    { beername : 'St. Arnolds', Type : 'American-style Amber'},
+    { beername : 'Corona', Type : 'light ale'},
+    { beername : 'Fosters', Type : 'Lager'}
+];
+
+console.log(customers);
+
