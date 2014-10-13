5775f9eea863e286b5d3101b0f1d7b92626fce9a
therinken <admin@therinken.com>
2014-10-13 18:18:09 -0500

    a few changes to 1 through 4

--- hw.js
+++ hw.js
@@ -1,11 +1,44 @@
-// Number 1 Write your own forEach function (like we did in class). It takes the array and the function (referred to as a callback) as arguments:
-// Number 2 Write your own map, reduce, and filter functions that use your custom forEach to do their work.
-// function forEach(list, callback) {
-//         for (var i = 0; len = list.length; i++) {
-//              callback(list[i], i, list);
-//              }
-// }
+// Number 1 Write your own forEach function (like we did in class). It 
+// takes the array and the function (referred to as a callback) as 
+// arguments:
+// Number 2 Write your own map, reduce, and filter functions that use your 
+// custom forEach to do their work.
 // 
+// 
+function forEach(list, callback) {
+    for (var i = 0; i < beerList.length; i++) {
+        callback(list[i], i, list);
+    }
+
+    function map(list, callback) {
+        var newBrew = [];
+        forEach(list, function(val, i, arr) {
+            newBrew.push(callback(val, i, arr));
+        })
+        return newBrew;
+    }
+
+    function reduce(list, callback, initialValue) {
+        var value, prevVal = initialValue;
+        if (initialValue) beerList.unshift(initialValue);
+        forEach(list, function(val, i, arr) {
+            if (i === 0) return;
+            prevVal = callback(prevVal, arr[i], i, arr);
+        });
+        return prevVal;
+    }
+
+    function filter(list, callback) {
+        var newBrew = [];
+        forEach(list, function(val, i, arr) {
+            if (callback(val, i, arr)) {
+                newBrew.push(val);
+            }
+        });
+        return newBrew;
+    }
+};
+
 var beerList = [{
     beername: 'Corona',
     beertype: 'Ale'
@@ -20,6 +53,9 @@ var beerList = [{
     beertype: 'IPA'
 }];
 
+
+
+/*
 beerList.forEach(function(c) {
     var brewski = beerList.filter(function(c) {
             if (c.beername.length > 0) {
@@ -39,9 +75,10 @@ beerList.forEach(function(c) {
 
     console.log(c);
 
-})
+})*/
 
-// Number 3 Write a function called pluck that extracts a list of property names:
+// Number 3 Write a function called pluck that extracts a few feathers from
+// a chickens rear quarters (...without being seen or heard).
 var stooges = [{
     name: 'moe',
     age: 40
@@ -53,7 +90,22 @@ var stooges = [{
     age: 60
 }];
 
+function pluck(list, propertyName) {
+    var newList = [];
+    forEach(list, function(val, i, arr) {
+            newList.push(val[propertyName]);
+    })
+    return newList;
+}
 
+console.dir(pluck(stooges, 'name'));
+
+/**
+ * following is but another version I had offered up to the invisible
+ * ones moments before Masta_Matt's venerable wisdom had wrought the
+ * age of enlightenment upon my silly, befuddled brain.
+ *
+ */
 function pluck(a) {
     var names = a.map(function(element) {
         return element.name;
@@ -62,79 +114,64 @@ function pluck(a) {
 }
 console.log(pluck(stooges));
 
-// could use Underscore.js... "_.pluck(stooges, name);"  .... but that might be considered cheating? => ["moe", "larry", "curly"]
-/**
- * Julie did long-hand version.... like this! just seems way too much for a simple action.
- * 
- * var stooges = [{
-    name: "moe",
-    age: 40
-}, {
-    name: "larry",
-    age: 50
-}, {
-    name: "curly",
-    age: 60
-}];
 
-function filter(array, test) {
-    var passed = [];
-    for (var i = 0; i < array.length; i++) {
-        if (test(array[i]))
-            passed.push(array[i]);
-    }
-    return passed;
+//Number 4. Write a function called reject that makes an otherwise happy
+//individual feel upset for a few moments, whereupon he/she realizes (as 
+//though a mighty cloud of unknowing had been dispelled) that it was all
+//folly and thereby has a jolly good laughing!
 
+function reject(list, callback) {
+    var newList = [];
+    forEach(list, function(val, i, arr) {
+        if (!callback(val, i, arr)) {
+            newList.push(val);
+        }
+    })
+    return newList;
 }
 
-function map(array, transform) {
-    var mapped = [];
-    for (var i = 0; i < array.length; i++)
-        mapped.push(transform(array[i]));
-    return mapped;
-}
-
-var pluck = stooges.filter(function(sto) {
-    return sto.name;
+var odds = reject([1, 2, 3, 4, 5, 6], function(num) {
+    return num % 2 == 0;
 });
-
-console.log(map(pluck, function(sto) {
-    return sto.name;
-}));
+console.log(odds);
+/**
+ * Following is another attempt by me on the elusive method
+ * (dubbed by the great masters... 'reduce') that was found to be 
+ * acceptable but a smidgen out of context and so, was thereby imprisoned 
+ * in a tiny cell meant for the little peoples for what i think was 
+ * a few happy years!
  */
-
-//Number 4 Write a function called reject that returns the values in list without the elements that the truth test (predicate) passes. The opposite of filter.
-
 var arr = [1, 2, 3, 4, 5, 6];
 var remove = [];
 
 function reject() {
     var i;
     for (i = 0; i < arr.length; i++) {
-        if (Number(arr[i]) != i % 2) { // modulo to seperate that which is not divisible by two 
-            remove.push(arr[i]); // Pushing the results from if
-            arr.splice(i, 1); // Removing the parts of arr that don't match our if
+        if (Number(arr[i]) != i % 2) {
+            remove.push(arr[i]);
+            arr.splice(i, 1);
         }
     }
-};
-
-reject(); // Calling the function.
+}
+reject();
 console.log(remove);
 
-
-
-// Number 5 Write a function called find that looks through each value in the list, returning the first one that passes a truth test (predicate), or undefined if no value passes the test. The function returns as soon as it finds an acceptable element, and doesn't traverse the entire list.
-var even = function(list, predicate) {
-    for (var i = 0; i < list.length; i++) {
-        if (predicate(list[i]) === true) {
-            return list[i];
-        }
-    }
-    return undefined;
+// Number 5 Write a function called find that looks through each value in 
+// the list, returning the first one that passes a truth test (predicate), 
+// or undefined if no value passes the test. The function returns as soon 
+// as it finds an acceptable element, and doesn't traverse the entire list.
+// 
+var even = function(list, predicate) {  
+    for (var i = 0; i < list.length; i++) {    
+        if (predicate(list[i]) === true) {      
+            return list[i];    
+        }  
+    }  
+    return undefined;
 }
 
-var predicate = function(first) {
-    return first % 2 == 0;
+var predicate = function(first) {  
+    return first % 2 == 0;
 }
 
 var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -142,29 +179,39 @@ var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 console.log(even(arr, predicate));
 
 
-//Number 6 Write a function called where that looks through each value in the list, 
-//returning an array of all the values that contain all of the key-value pairs listed in properties.
-function where(list, properties) { 
- var listOfPlays = [{title: "Cymbeline", author: "Shakespeare", year: 1611},
-                   {title: "The Tempest", author: "Shakespeare", year: 1611}];
-                return listOfPlays[1];
-
-                
+//Number 6 Write a function called where that looks through each value in 
+//the list, returning an array of all the values that contain all of the 
+//key-value pairs listed in properties.
+//
 function where(list, properties) {
-    var author = listOfPlays.author;
-    var year = listOfPlays.year;
-    var properties = author + year;
+    var listOfPlays = [{
+        title: "Cymbeline",
+        author: "Shakespeare",
+        year: 1611
+    }, {
+        title: "The Tempest",
+        author: "Shakespeare",
+        year: 1611
+    }];
+    return listOfPlays[1];
 
-    return properties;
-};
+
+    function where(list, properties) {
+        var author = listOfPlays.author;
+        var year = listOfPlays.year;
+        var properties = author + year;
+
+        return properties;
+    };
 
 }
-                return (listOfPlays.author,listOfPlays.year);
+return (listOfPlays.author, listOfPlays.year);
 };
-                console.log(where(listOfPlays));
+console.log(where(listOfPlays));
 
 
-/* Do stuff */ }
+/* Do stuff */
+}
 
 where(listOfPlays, {
     author: "Shakespeare",
